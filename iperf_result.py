f627dac02d8996de3fe826ccaba2f275fe31a838
Shane Hull <shaneh@Shanes-MBP.noddy.mckinnonsc.vic.edu.au>
2018-09-17 15:01:16 +1000

    add iperf script

---
+++ iperf_result.py
@@ -0,0 +1,10 @@
+import iperf3
+
+client = iperf3.Client()
+client.duration = 5
+client.server_hostname = 'speedtest.serverius.net'
+client.port = 5002
+result = client.run()
+result.sent_Mbps
+
+print("%.2f" % result.sent_Mbps)
